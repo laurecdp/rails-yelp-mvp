b2051d4505d8b12c1f9173b50dd47fb11ed4a42e
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-10 11:17:29 +0100

    rails-yelp-mvp models correction avec spec

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true, less_than: 5 }
 end
