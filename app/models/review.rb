da1422fbf6e0cf8d388b26bd947c8200edfa1ce6
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-02-10 11:26:24 +0100

    rails-yelp-mvp models bons

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ApplicationRecord
 
   validates :content, presence: true
   validates :rating, presence: true
-  validates :rating, numericality: { only_integer: true, less_than: 5 }
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: 0..5 }
 end
